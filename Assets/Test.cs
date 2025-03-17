2e65c1dae327c7d370d119fee306206f657f3144
KBW <151644866+KBWGamess@users.noreply.github.com>
2025-03-17 19:13:21 +0300

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Гоша молодец
+        // Net ne molodec
     }
 
     // Update is called once per frame
